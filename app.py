02d61ca4e0bfecf4aa81467ad19c80595c779fe5
ak1302 <53116533+ak1302@users.noreply.github.com>
2022-04-13 02:37:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+from __future__ import print_function
 import sys
 from flask import Flask, request, redirect, url_for, render_template, session, send_from_directory, send_file
 from werkzeug.utils import secure_filename
